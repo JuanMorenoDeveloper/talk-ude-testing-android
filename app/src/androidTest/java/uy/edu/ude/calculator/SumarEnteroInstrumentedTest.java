de5fe1ac3452b630795cc523a9dcaf82e60687f4
Juan Moreno <earth001@gmail.com>
2018-06-08 18:00:26 -0300

    Nuevo contenido

--- app/src/androidTest/java/uy/edu/ude/calculator/SumarEnteroInstrumentedTest.java
+++ app/src/androidTest/java/uy/edu/ude/calculator/SumarEnteroInstrumentedTest.java
@@ -2,7 +2,6 @@ package uy.edu.ude.calculator;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
